fdf67a27045c76b255f00169b335e8b910b2508b
Ivan Mordovets <imordovets@gmail.com>
2025-09-16 16:47:29 +0200

    testing runners

--- main/views.py
+++ main/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Ivan Hello, world! This is my Django index page. TEST ")
+    return HttpResponse("Ivan world Hello, world! This is my Django index page. TEST ")
