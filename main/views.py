3520e192fa247b7b6206051bceb1b127454b9908
Ivan Mordovets <imordovets@gmail.com>
2025-09-16 16:34:27 +0200

    test1

--- main/views.py
+++ main/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world! This is my Django index page. TEST TEST")
+    return HttpResponse("Hello, world! This is my Django index page. TEST ")
