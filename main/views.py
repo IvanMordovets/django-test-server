8508e09b86ba4c488d5f6fdc304d1567db840e69
Ivan Mordovets <imordovets@gmail.com>
2025-09-16 16:44:37 +0200

    ivan

--- main/views.py
+++ main/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world! This is my Django index page. TEST ")
+    return HttpResponse("Ivan Hello, world! This is my Django index page. TEST ")
